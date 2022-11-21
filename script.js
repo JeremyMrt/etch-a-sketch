22c8e532c247ea4bbfe38fe8106b9c1f3d5e4188
Jeremy Martin <jeremy.mrti@gmail.com>
2022-11-21 17:41:49 +0100

    Mouse over event colors in black

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const theGrid = document.querySelector(".grid-container");
 
-let numberGrid = 16;
+let numberGrid = 514;
 let sqrtRootGrid = Math.floor(Math.sqrt(numberGrid));
 let gridSize = 300 / sqrtRootGrid;
+
 
 for (let i=0 ; i< numberGrid ; i++) {
     const oneBox = document.createElement('div');
@@ -12,3 +13,8 @@ for (let i=0 ; i< numberGrid ; i++) {
 
 theGrid.style.gridTemplateColumns = `repeat(${sqrtRootGrid}, ${gridSize}px`;
 theGrid.style.gridTemplateRows = `repeat(${sqrtRootGrid}, ${gridSize}px`;
+
+theGrid.addEventListener('mouseover', (e)=> {
+    console.log(e)
+    e.target.classList.add('mouse-over');
+})
